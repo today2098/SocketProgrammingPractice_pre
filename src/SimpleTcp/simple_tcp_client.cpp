1674267fffc8745a3e2e5ed7f54df67021ebd289
Today <ipp.30042022@gmail.com>
2022-09-28 23:22:58 +0900

    [update] Add a function "DieWithSystemMessage()" for error handling

--- src/SimpleTcp/simple_tcp_client.cpp
+++ src/SimpleTcp/simple_tcp_client.cpp
@@ -1,38 +1,47 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#include <cstdio>
+#include <cstring>
+
+#include "../library/utility.hpp"
 
 int main() {
     struct sockaddr_in server;
     int sock;
     char buf[32];
     int n;
+    int res;
 
     /* ソケットの作成 */
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1) DieWithSystemMessage("socket()");
 
     /* 接続先指定用構造体の準備 */
     server.sin_family = AF_INET;
     server.sin_port = htons(12345);
 
     /* 127.0.0.1はlocalhost */
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr.s_addr);
+    res = inet_pton(AF_INET, "127.0.0.1", &server.sin_addr.s_addr);
+    if(res == -1) DieWithSystemMessage("inet_pton()");
 
     /* サーバに接続 */
-    connect(sock, (struct sockaddr *)&server, sizeof(server));
+    res = connect(sock, (struct sockaddr *)&server, sizeof(server));
+    if(res == -1) DieWithSystemMessage("connect()");
 
     /* サーバからデータを受信 */
     memset(buf, 0, sizeof(buf));
     n = read(sock, buf, sizeof(buf));
+    if(n == -1) DieWithSystemMessage("read()");
 
-    printf("%d, %s\n", n, buf);
+    printf("Message: %s\nSize: %d Byte\n", buf, n);
 
     /* socketの終了 */
-    close(sock);
+    res = close(sock);
+    if(res == -1) DieWithSystemMessage("close()");
 
     return 0;
 }
--- src/SimpleTcp/simple_tcp_server.cpp
+++ src/SimpleTcp/simple_tcp_server.cpp
@@ -1,8 +1,11 @@
 #include <netinet/in.h>
-#include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#include <cstdio>
+
+#include "../library/utility.hpp"
 
 int main() {
     int sock0;
@@ -10,31 +13,39 @@ int main() {
     struct sockaddr_in client;
     socklen_t len;
     int sock;
+    int res;
 
     /* ソケットの作成 */
-    sock0 = socket(AF_INET, SOCK_STREAM, 0);
+    sock0 = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock0 == -1) DieWithSystemMessage("socket()");
 
     /* ソケットの設定 */
     addr.sin_family = AF_INET;
     addr.sin_port = htons(12345);
     addr.sin_addr.s_addr = INADDR_ANY;
-    bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+    res = bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+    if(res == -1) DieWithSystemMessage("bind()");
 
     /* TCPクライアントからの接続要求を待てる状態にする */
-    listen(sock0, 5);
+    res = listen(sock0, 5);
+    if(res == -1) DieWithSystemMessage("listen()");
 
     /* TCPクライアントからの接続要求を受け付ける */
     len = sizeof(client);
     sock = accept(sock0, (struct sockaddr *)&client, &len);
+    if(sock == -1) DieWithSystemMessage("accept()");
 
     /* 5文字送信 */
-    write(sock, "HELLO", 5);
+    res = write(sock, "HELLO", 5);
+    if(res == -1) DieWithSystemMessage("write()");
 
     /* TCPセッションの終了 */
-    close(sock);
+    res = close(sock);
+    if(res == -1) DieWithSystemMessage("close()");
 
     /* listen するsocketの終了 */
-    close(sock0);
+    res = close(sock0);
+    if(res == -1) DieWithSystemMessage("close()");
 
     return 0;
 }
