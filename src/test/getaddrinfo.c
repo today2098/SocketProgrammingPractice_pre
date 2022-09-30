2bddf150338b2fadebe770d2150f1659c5848174
Today <ipp.30042022@gmail.com>
2022-09-30 10:05:04 +0900

    [update] Add an argument for hostname

--- src/test/getaddrinfo.c
+++ src/test/getaddrinfo.c
@@ -5,27 +5,38 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-int main() {
-    char *hostname = "localhost";
-    // char *hostname = "DESKTOP-RHINHNB";
-    struct addrinfo hints, *res;
+int main(int argc, char *argv[]) {
+    char hostname[] = "localhost";
+    // char hostname[] = "DESKTOP-RHINHNB";
+    struct addrinfo hints, *res0, *res;
     struct in_addr addr;
     char buf[16];
     int err;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_family = AF_INET;
-    if((err = getaddrinfo(hostname, NULL, &hints, &res)) != 0) {
-        fprintf(stderr, "error %d\n", err);
+    if(argc > 2) {
+        fprintf(stderr,
+                "Test getaddrinfo()\n\n"
+                "Usage:\n\t%s [hostname]\n\n"
+                "Example:\n\t%s localhost\n\t%s google.com\n",
+                argv[0], argv[0], argv[0]);
         return 1;
     }
 
-    addr.s_addr = ((struct sockaddr_in *)(res->ai_addr))->sin_addr.s_addr;
-    inet_ntop(AF_INET, &addr, buf, sizeof(buf));
-    printf("ip address : %s\n", buf);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET;
+    if((err = getaddrinfo((argc == 2 ? argv[1] : hostname), NULL, &hints, &res0)) != 0) {
+        fprintf(stderr, "error %d: %s\n", err, gai_strerror(err));
+        return 1;
+    }
 
-    freeaddrinfo(res);
+    for(res = res0; res; res = res->ai_next) {
+        addr.s_addr = ((struct sockaddr_in *)(res->ai_addr))->sin_addr.s_addr;
+        inet_ntop(AF_INET, &addr, buf, sizeof(buf));
+        printf("ip address : %s\n", buf);
+    }
+
+    freeaddrinfo(res0);
 
     return 0;
 }
