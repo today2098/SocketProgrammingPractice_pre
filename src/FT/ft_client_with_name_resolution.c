7e73d6f9387f2e2de5c7cdf2e265fc0240a47fc8
Today <ipp.30042022@gmail.com>
2022-09-30 23:35:40 +0900

    [add] Add an argument for filename to "ft_client.c"

---
+++ src/FT/ft_client_with_name_resolution.c
@@ -0,0 +1,83 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../library/utility.h"  // for DieWithSystemMessage().
+
+int main(int argc, char *argv[]) {
+    char *hostname, *filename;
+    int fd;
+    struct addrinfo hints, *res0, *res;
+    int sock;
+    char buf[65536];
+    int n;
+    int ret;
+
+    if(argc != 3) {
+        fprintf(stderr,
+                "File transport client with name resolution\n\n"
+                "Usage:\n\t%s <hostname> <filename>\n\n"
+                "Example:\n\t%s localhost sample/water.jpg\n",
+                argv[0], argv[0]);
+        return 1;
+    }
+
+    hostname = argv[1];
+    filename = argv[2];
+
+    fd = open(filename, O_RDONLY);
+    if(fd == -1) DieWithSystemMessage("open()");
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_UNSPEC;  // プロトコルは指定しない．IPv4 or IPv6.
+    ret = getaddrinfo(hostname, "12345", &hints, &res0);
+    if(ret != 0) {
+        fprintf(stderr, "error %d: %s\n", ret, gai_strerror(ret));
+        DieWithSystemMessage("getaddrinfo()");
+    }
+
+    /* addrinfoリストの要素を先頭から接続できるまで順に試行する */
+    for(res = res0; res; res = res->ai_next) {
+        // ソケットの作成．
+        sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if(sock == -1) continue;
+
+        /* ソケットが生成できたらサーバへ接続を試みる */
+        ret = connect(sock, res->ai_addr, res->ai_addrlen);
+        if(ret == -1) {  // 失敗したらリストの次の項目を試す．
+            close(sock);
+            sock = -1;
+            continue;
+        }
+
+        break;  // ここではすでに接続に成功しているので，forループを脱出．
+    }
+
+    freeaddrinfo(res0);
+
+    /* リストのすべての項目が失敗していないかチェック */
+    if(sock == -1) {
+        fprintf(stderr, "Cannot connect %s\n", hostname);
+        return 1;
+    }
+
+    while((n = read(fd, buf, sizeof(buf))) > 0) {
+        ret = write(sock, buf, n);
+        if(ret < 1) {
+            perror("write()");
+            break;
+        }
+    }
+
+    /* socketの終了 */
+    close(sock);
+
+    return 0;
+}
